ad373e95675b9c740eaba95cc4bdbe10c23f3f10
YodaMH/Marcel <marcel.hauser2@outlook.de>
2023-05-15 13:21:58 +0200

    js.js

--- Website/JS.js
+++ Website/JS.js
@@ -23,79 +23,47 @@ http.createServer(function (req, res) {
   });
 }).listen(8080);*/
 //Require in some of the native stuff that comes with Node
-var http = require('http');
-var url = require('url');
-var path = require('path');
-var fs = require('fs');
 
-// Port number to use
-var port = 8080;
-// Colors for CLI output
-var WHITE = '\033[39m';
-var RED = '\033[91m';
-var GREEN = '\033[32m';
+const express = require('express');
+const http = require('http');
+const fs = require('fs');
+const multer  = require('multer');
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const app = express();
+const port = 8080;
 
-// Create the server
-http.createServer(function (request, response) {
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,)));
 
-    // The requested URL, like http://localhost:8000/file.html => /file.html
-    var uri = url.parse(request.url).pathname;
-    // get the /file.html from above and then find it from the current folder
-    var filename = path.join(process.cwd(), uri);
+// Definiere eine Route für die Startseite
+app.get('/', (req, res) => {
+  // Sende die HTML-Datei als Antwort auf die Anfrage
+  res.sendFile(path.join(__dirname,'Main.html'));
+  
+});
 
-    // Setting up MIME-Type (YOU MAY NEED TO ADD MORE HERE) <--------
-    var contentTypesByExtension = {
-        '.html': 'text/html',
-        '.css':  'text/css',
-        '.js':   'text/javascript',
-        '.json': 'text/json',
-        '.svg':  'image/svg+xml'
-    };
-
-    // Check if the requested file exists
-    fs.exists(filename, function (exists) {
-        // If it doesn't
-        if (!exists) {
-            // Output a red error pointing to failed request
-            console.log(RED + 'FAIL: ' + filename);
-            // Redirect the browser to the 404 page
-            filename = path.join(process.cwd(), '/404.html');
-        // If the requested URL is a folder, like http://localhost:8000/catpics
-        } else if (fs.statSync(filename).isDirectory()) {
-            // Output a green line to the console explaining what folder was requested
-            console.log(GREEN + 'FLDR: ' + WHITE + filename);
-            // redirect the user to the index.html in the requested folder
-            filename += '/index.html';
-        }
-
-        // Assuming the file exists, read it
-        fs.readFile(filename, 'binary', function (err, file) {
-            // Output a green line to console explaining the file that will be loaded in the browser
-            console.log(GREEN + 'FILE: ' + WHITE + filename);
-            // If there was an error trying to read the file
-            if (err) {
-                // Put the error in the browser
-                response.writeHead(500, {'Content-Type': 'text/plain'});
-                response.write(err + '\n');
-                response.end();
-                return;
-            }
-
-            // Otherwise, declare a headers object and a var for the MIME-Type
-            var headers = {};
-            var contentType = contentTypesByExtension[path.extname(filename)];
-            // If the requested file has a matching MIME-Type
-            if (contentType) {
-                // Set it in the headers
-                headers['Content-Type'] = contentType;
-            }
-
-            // Output the read file to the browser for it to load
-            response.writeHead(200, headers);
-            response.write(file, 'binary');
-            response.end();
-        });
-
-    });
-
-}).listen(parseInt(port, 10));
+  /* SQLite-Datenbank öffnen
+  let db = new sqlite3.Database('DB/data.db');
+  
+  // Route zum Hochladen des Bilds
+  app.post('/upload/user', (req, res) => {
+    // Informationen über das hochgeladene Bild erhalten
+    /*const name = req.body.userName;
+    const email = req.body.userEmail;
+    const passwort = req.body.userPasswort;*/
+    //console.log(req.body);
+    /*// Bildinformationen in die SQLite-Datenbank schreiben
+    db.run('INSERT INTO user (Name,Email,Passwort) VALUES ('+name+','+email+','+passwort+')'), (err) => {
+      if (err) {
+        console.log(err);
+        res.sendStatus(500);
+      } else {
+        res.sendStatus(200);
+      }
+    }}); */
+  
+  // Server starten
+  app.listen(port, () => {
+    console.log(`Server läuft auf http://localhost:${port}`);
+  });
